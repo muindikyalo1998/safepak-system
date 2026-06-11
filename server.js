0bc47de2785801277b972d07d8e8e66aae00bd30
muindikyalo1998 <muindikyalo1998@gmail.com>
2026-06-11 12:25:07 +0300

    final railway production fix

--- server.js
+++ server.js
@@ -35,14 +35,14 @@ app.get("/test", (req, res) => {
   res.send("TEST OK");
 });
 
-/* ================= DB ================= */
+/* ================= DB (SAFE VERSION) ================= */
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "test",
+  port: process.env.DB_PORT || 3306
 });
 
 /* ================= SECRET ================= */
